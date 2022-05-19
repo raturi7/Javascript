6c4659677e74c476e77a87fb9a0a00a0e7b995c8
Raturi7 <79138283+raturi7@users.noreply.github.com>
2022-05-19 10:43:34 +0530

    Create Temperature .js
    Here, the Temperature is converted from Kelvin to degrees Celsius and then to Fahrenheit. Afterward, the Temperature is converted from degrees Celsius to Newton Scale.

---
+++ Temperature .js
@@ -0,0 +1,10 @@
+//kelvin to celsius
+var kelvin = 0;
+var celsius = kelvin - 273; // converted in celsius from kelvin
+//celsius to fahrenheit
+var fahrenheit = celsius*(9/5)+32; // converted in fahrenheit from celsius
+fahrenheit = Math.floor(fahrenheit); // converted to its nearest floor value
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+var Newton = celsius * (33/100);
+Newton = Math.floor(Newton);
+console.log(`Thw temperature is ${Newton} Newton`)
